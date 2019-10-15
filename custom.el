6f892e935e118cfaff500b4dcc25cbf2cc462010
grburgess <jmichaelburgess@gmail.com>
2019-10-15 16:28:23 +0200

    updates

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (elpy zweilight-theme zerodark-theme zeno-theme yasnippet-snippets yasnippet-classic-snippets yaml-mode which-key warm-night-theme use-package undo-tree twilight-anti-bright-theme toxi-theme sublimity sublime-themes standoff-mode stan-snippets ssh srcery-theme spotify sphinx-doc spacemacs-theme soothe-theme soft-charcoal-theme smex smart-mode-line-powerline-theme smart-mode-line-atom-one-dark-theme slime shell-pop sexy-monochrome-theme seti-theme rope-read-mode rimero-theme reykjavik-theme rainbow-identifiers rainbow-delimiters pyvenv python-docstring projectile planet-theme paredit paganini-theme overcast-theme origami org-ref org-cliplink org-bullets omtose-phellack-theme olivetti nyx-theme nova-theme northcode-theme noctilux-theme nimbus-theme neotree mutt-mode multiple-cursors mu4e-maildirs-extension mu4e-conversation mood-one-theme monokai-alt-theme monochrome-theme moe-theme minimal-theme metalheart-theme majapahit-theme magithub magit-popup madhat2r-theme lush-theme liso-theme kooten-theme klere-theme kaolin-themes js2-mode jedi iedit idle-require idea-darkula-theme highlight-numbers highlight-indentation highlight-indent-guides hamburg-theme gruber-darker-theme grandshell-theme gotham-theme git-gutter-fringe gist forest-blue-theme foggy-night-theme focus flymake-python-pyflakes flycheck flatui-dark-theme find-file-in-project eziam-theme exotica-theme ess easy-jekyll doom-modeline dockerfile-mode distinguished-theme diminish darktooth-theme darkokai-theme darkmine-theme dark-mint-theme dark-krystal-theme dakrone-theme cython-mode cyberpunk-theme cyberpunk-2019-theme context-coloring constant-theme company-reftex company-jedi company-irony-c-headers company-auctex color-identifiers-mode colonoscopy-theme clipboard-collector calmer-forest-theme bug-hunter browse-kill-ring blacken birds-of-paradise-plus-theme badger-theme auto-complete-auctex auto-compile atom-one-dark-theme atom-dark-theme arjen-grey-theme arc-dark-theme apropospriate-theme ample-theme alect-themes afternoon-theme ace-jump-mode abyss-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
