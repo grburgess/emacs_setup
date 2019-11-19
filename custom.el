7bc0e8bd636b1b1ab2979e37edf99c00c534092c
grburgess <jmichaelburgess@gmail.com>
2019-11-19 10:11:22 +0100

    rm custom.el

--- custom.el
+++ custom.el
@@ -3,7 +3,81 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#000000" "#8b0000" "#00ff00" "#ffa500" "#7b68ee" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(background-color "#202020")
+ '(background-mode dark)
+ '(compilation-message-face (quote default))
+ '(cursor-color "#cccccc")
+ '(custom-safe-themes
+   (quote
+    ("10a31b6c251640d04b2fa74bd2c05aaaee915cbca6501bcc82820cdc177f5a93" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "b583823b9ee1573074e7cbfd63623fe844030d911e9279a7c8a5d16de7df0ed0" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "47ec21abaa6642fefec1b7ace282221574c2dd7ef7715c099af5629926eb4fd7" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "071f5702a5445970105be9456a48423a87b8b9cfa4b1f76d15699b29123fb7d8" "72559f34f255a17b7055687c1593ae1dbbd3c5b61b56d0ca96f9468ca228b96c" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "0574ebcddc942ac17a864d1cf1093de5b4f1ff5b0fc68ad1cfe1aff28f732ba2" "ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" "669e02142a56f63861288cc585bee81643ded48a19e36bfdf02b66d745bcc626" "04dd0236a367865e591927a3810f178e8d33c372ad5bfef48b5ce90d4b476481" "7356632cebc6a11a87bc5fcffaa49bae528026a78637acd03cae57c091afd9b9" "cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" "c7f10959cb1bc7a36ee355c765a1768d48929ec55dde137da51077ac7f899521" "de9fa4b3614611bed2fe75e105bd0d37542924b977299736f158dd4d7343c666" "ba72dfc6bb260a9d8609136b9166e04ad0292b9760a3e2431cf0cd0679f83c3a" "41098e2f8fa67dc51bbe89cce4fb7109f53a164e3a92356964c72f76d068587e" "0d456bc74e0ffa4bf5b69b0b54dac5104512c324199e96fc9f3a1db10dfa31f3" "37ba833442e0c5155a46df21446cadbe623440ccb6bbd61382eb869a2b9e9bf9" "462d6915a7eac1c6f00d5acd8b08ae379e12db2341e7d3eac44ff7f984a5e579" "2593436c53c59d650c8e3b5337a45f0e1542b1ba46ce8956861316e860b145a0" "0329c772ed96053a73b9ddddf96c1183e23c267955bbdf78e7933057ce9da04b" "e7b49145d311e86da34a32a7e1f73497fa365110a813d2ecd8105eaa551969da" "ba913d12adb68e9dadf1f43e6afa8e46c4822bb96a289d5bf1204344064f041e" "423435c7b0e6c0942f16519fa9e17793da940184a50201a4d932eafe4c94c92d" "cdb3e7a8864cede434b168c9a060bf853eeb5b3f9f758310d2a2e23be41a24ae" default)))
+ '(diary-entry-marker (quote font-lock-variable-name-face))
+ '(emms-mode-line-icon-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *note[] = {
+/* width height num_colors chars_per_pixel */
+\"    10   11        2            1\",
+/* colors */
+\". c #1fb3b3\",
+\"# c None s None\",
+/* pixels */
+\"###...####\",
+\"###.#...##\",
+\"###.###...\",
+\"###.#####.\",
+\"###.#####.\",
+\"#...#####.\",
+\"....#####.\",
+\"#..######.\",
+\"#######...\",
+\"######....\",
+\"#######..#\" };")))
+ '(fci-rule-color "#383838" t)
+ '(foreground-color "#cccccc")
+ '(frame-brackground-mode (quote dark))
+ '(gnus-logo-colors (quote ("#2fdbde" "#c0c0c0")) t)
+ '(gnus-mode-line-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *gnus-pointer[] = {
+/* width height num_colors chars_per_pixel */
+\"    18    13        2            1\",
+/* colors */
+\". c #1fb3b3\",
+\"# c None s None\",
+/* pixels */
+\"##################\",
+\"######..##..######\",
+\"#####........#####\",
+\"#.##.##..##...####\",
+\"#...####.###...##.\",
+\"#..###.######.....\",
+\"#####.########...#\",
+\"###########.######\",
+\"####.###.#..######\",
+\"######..###.######\",
+\"###....####.######\",
+\"###..######.######\",
+\"###########.######\" };")) t)
+ '(highlight-changes-colors (quote ("#ff8eff" "#ab7eff")))
+ '(highlight-tail-colors
+   (quote
+    (("#323342" . 0)
+     ("#63de5d" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#323342" . 100))))
+ '(jdee-db-active-breakpoint-face-colors (cons "#10151C" "#5EC4FF"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#10151C" "#8BD49C"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#10151C" "#384551"))
  '(linum-format " %7i ")
+ '(magit-diff-use-overlays nil)
+ '(objed-cursor-color "#D95468")
  '(org-emphasis-alist
    (quote
     (("*" bold)
@@ -15,10 +89,47 @@
       (:strike-through t)))))
  '(package-selected-packages
    (quote
-    (swiper-helm counsel ivy-explorer zweilight-theme zerodark-theme zeno-theme yasnippet-snippets yasnippet-classic-snippets yaml-mode which-key warm-night-theme use-package undo-tree twilight-anti-bright-theme toxi-theme sublimity sublime-themes standoff-mode stan-snippets ssh srcery-theme spotify sphinx-doc spacemacs-theme soothe-theme soft-charcoal-theme smex smart-mode-line-powerline-theme smart-mode-line-atom-one-dark-theme slime shell-pop sexy-monochrome-theme seti-theme rope-read-mode rimero-theme reykjavik-theme rainbow-identifiers rainbow-delimiters python-docstring projectile planet-theme paredit paganini-theme overcast-theme origami org-ref org-cliplink org-bullets omtose-phellack-theme olivetti nyx-theme nova-theme northcode-theme noctilux-theme nimbus-theme neotree mutt-mode multiple-cursors mu4e-maildirs-extension mu4e-conversation mood-one-theme monokai-alt-theme monochrome-theme moe-theme minimal-theme metalheart-theme majapahit-theme magithub magit-popup madhat2r-theme lush-theme liso-theme kooten-theme klere-theme kaolin-themes js2-mode jedi iedit idle-require idea-darkula-theme highlight-numbers highlight-indent-guides hamburg-theme gruber-darker-theme grandshell-theme gotham-theme git-gutter-fringe gist forest-blue-theme foggy-night-theme focus flymake-python-pyflakes flycheck flatui-dark-theme eziam-theme exotica-theme ess elpygen elpy easy-jekyll doom-themes doom-modeline dockerfile-mode distinguished-theme diminish darktooth-theme darkokai-theme darkmine-theme dark-mint-theme dark-krystal-theme dakrone-theme cython-mode cyberpunk-theme cyberpunk-2019-theme crux context-coloring constant-theme company-reftex company-jedi company-irony-c-headers company-auctex color-identifiers-mode colonoscopy-theme calmer-forest-theme bug-hunter browse-kill-ring blacken birds-of-paradise-plus-theme beacon badger-theme auto-complete-auctex auto-compile atom-one-dark-theme atom-dark-theme arjen-grey-theme arc-dark-theme apropospriate-theme ample-theme alect-themes afternoon-theme ace-jump-mode abyss-theme))))
+    (darkroom flycheck-pyflakes counsel-spotify fancy-battery doom-modeline ivy-youtube suscolors-theme all-the-icons-ivy org-variable-pitch gruvbox-theme phoenix-dark-mono-theme darkburn-theme ewal-spacemacs-themes org-beautify-theme swiper swiper-helm counsel ivy-explorer zweilight-theme zerodark-theme zeno-theme yasnippet-snippets yasnippet-classic-snippets yaml-mode which-key warm-night-theme use-package undo-tree twilight-anti-bright-theme toxi-theme sublimity sublime-themes standoff-mode stan-snippets ssh srcery-theme spotify sphinx-doc spacemacs-theme soothe-theme soft-charcoal-theme smex smart-mode-line-powerline-theme smart-mode-line-atom-one-dark-theme slime shell-pop sexy-monochrome-theme seti-theme rope-read-mode rimero-theme reykjavik-theme rainbow-identifiers rainbow-delimiters python-docstring projectile planet-theme paredit paganini-theme overcast-theme origami org-ref org-cliplink org-bullets omtose-phellack-theme olivetti nyx-theme nova-theme northcode-theme noctilux-theme nimbus-theme neotree mutt-mode multiple-cursors mu4e-maildirs-extension mu4e-conversation mood-one-theme monokai-alt-theme monochrome-theme moe-theme minimal-theme metalheart-theme majapahit-theme magithub magit-popup madhat2r-theme lush-theme liso-theme kooten-theme klere-theme kaolin-themes js2-mode jedi iedit idle-require idea-darkula-theme highlight-numbers highlight-indent-guides hamburg-theme gruber-darker-theme grandshell-theme gotham-theme git-gutter-fringe gist forest-blue-theme foggy-night-theme focus flymake-python-pyflakes flatui-dark-theme eziam-theme exotica-theme ess easy-jekyll doom-themes dockerfile-mode distinguished-theme diminish darktooth-theme darkokai-theme darkmine-theme dark-mint-theme dark-krystal-theme dakrone-theme cython-mode cyberpunk-theme cyberpunk-2019-theme crux context-coloring constant-theme company-reftex company-jedi company-irony-c-headers company-auctex color-identifiers-mode colonoscopy-theme calmer-forest-theme bug-hunter browse-kill-ring blacken birds-of-paradise-plus-theme beacon badger-theme auto-complete-auctex auto-compile atom-one-dark-theme atom-dark-theme arjen-grey-theme arc-dark-theme apropospriate-theme ample-theme alect-themes afternoon-theme ace-jump-mode abyss-theme)))
+ '(pdf-view-midnight-colors (quote ("#fdf4c1" . "#1d2021")))
+ '(pos-tip-background-color "#E6DB74")
+ '(pos-tip-foreground-color "#242728")
+ '(vc-annotate-background "#1D252C")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#8BD49C")
+    (cons 40 "#abcd93")
+    (cons 60 "#cbc68b")
+    (cons 80 "#EBBF83")
+    (cons 100 "#e5ae6f")
+    (cons 120 "#df9e5b")
+    (cons 140 "#D98E48")
+    (cons 160 "#dc885f")
+    (cons 180 "#df8376")
+    (cons 200 "#E27E8D")
+    (cons 220 "#df7080")
+    (cons 240 "#dc6274")
+    (cons 260 "#D95468")
+    (cons 280 "#b05062")
+    (cons 300 "#884c5c")
+    (cons 320 "#604856")
+    (cons 340 "#56697A")
+    (cons 360 "#56697A")))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#242728" "#323342" "#F70057" "#ff0066" "#86C30D" "#63de5d" "#BEB244" "#E6DB74" "#40CAE4" "#06d8ff" "#FF61FF" "#ff8eff" "#00b2ac" "#53f2dc" "#f8fbfc" "#ffffff")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
+ '(default ((t (:background nil))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande" :height 2.0 :underline nil))))
+ '(org-headline-done ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande" :strike-through t))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#d4d4d6" :font "Lucida Grande")))))
